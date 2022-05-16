55641bd91108512950d7e033bdb79357ca0bef07
Do Xuan Dung <dungdx@pmr.vn>
2022-05-16 11:48:25 +0700

    initial

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,66 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {LoginPayload} from '../login-payload';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+import { AlertService } from 'src/app/alert.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  loginPayload: LoginPayload;
+  submitted = false;
+  loading = false;
+
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router,  
+    private formBuilder: FormBuilder,
+    private alertService: AlertService
+    ) {
+    this.loginForm = this.formBuilder.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]]
+  });
+    this.loginPayload = {
+      username: '',
+      password: ''
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+
+    if (this.loginForm.invalid) {
+      return;
+  }
+
+    this.loginPayload.username = this.f.username.value;
+    this.loginPayload.password = this.f.password.value;
+
+    this.authService.login(this.loginPayload).subscribe({
+      next: () => {
+          // get return url from query parameters or default to home page
+          console.log('login success');
+          this.router.navigateByUrl('/home');
+      },
+      error: error => {
+        this.alertService.error(error);
+        console.log('Login failed');
+        this.loading = false;
+      }
+  });
+  }
+}
---
+++ src/app/auth/register/register.component.ts
@@ -0,0 +1,60 @@
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {RegisterPayload} from '../register-payload';
+import {AuthService} from '../auth.service';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  registerForm: FormGroup;
+  registerPayload: RegisterPayload;
+  submitted = false;
+  loading = false;
+
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router:Router) {
+    this.registerForm = this.formBuilder.group({
+        username: [null, [Validators.required]],
+        email: [null, [Validators.required]],
+        password: [null, [Validators.required]],
+        confirmPassword: [null, [Validators.required], [Validators.minLength(6)]]
+    });
+    this.registerPayload = {
+      username: '',
+      email: '',
+      password: '',
+      confirmPassword: ''
+    };
+  }
+
+  ngOnInit() {
+  }
+
+  get f() { return this.registerForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+    this.loading = true;
+
+    if (this.registerForm.invalid) {
+      return;
+  }
+    this.registerPayload.username = this.registerForm.get('username').value;
+    this.registerPayload.email = this.registerForm.get('email').value;
+    this.registerPayload.password = this.registerForm.get('password').value;
+    this.registerPayload.confirmPassword = this.registerForm.get('confirmPassword').value;
+
+    this.authService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.router.navigateByUrl('/register-success');
+      },
+      error: error => {
+          this.loading = false;
+      }
+  });
+  }
+}
